fc7a8c8b6cd8a0072e25c7329be20a9caf30fb0a
Serhat Karaman <90270116+MasterofN0ne@users.noreply.github.com>
2024-05-01 18:05:34 +0300

    added all necessary files

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const filePath = "models.json";
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading JSON file:", err);
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (parseError) {
+      console.error("Error parsing JSON data:", parseError);
+    }
+  });
+});
+
+app.get(`/:brandName/`, (req, res) => {
+  const brandName = req.params.brandName;
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading JSON file:", err);
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      const brand = jsonData[brandName];
+      res.json(brand);
+    } catch (parseError) {
+      console.error("Error parsing JSON data:", parseError);
+    }
+  });
+});
+
+app.get(`/:brandName/:carModel/`, (req, res) => {
+  const brandName = req.params.brandName;
+  const carModel = req.params.carModel;
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading JSON file:", err);
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      const models = jsonData[brandName].modelSpecificProblems[carModel];
+      res.json(models);
+    } catch (parseError) {
+      console.error("Error parsing JSON data:", parseError);
+    }
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:3000/Acura/CL/`);
+});
